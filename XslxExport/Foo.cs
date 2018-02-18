5ef4830812f97e6d686b76ed53c6b3bd360d575c
winzarten <matus.celko47@gmail.com>
2018-02-18 16:45:41 +0100

    added XSLXExport project + tests project

--- XslxExport/Foo.cs
+++ XslxExport/Foo.cs
@@ -2,5 +2,9 @@
 {
     public class Foo
     {
+        public void ExportXSLX()
+        {
+            
+        }
     }
 }
---
+++ XslxExportTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("02f2803b-39f3-4c65-869d-80601d149954")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
