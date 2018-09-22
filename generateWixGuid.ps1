289a4148dd7c57b4076fabce66fa54a32d25cae9
winzarten <matus.celko47@gmail.com>
2018-09-22 19:33:27 +0200

    automatic guid generation for wix

---
+++ generateWixGuid.ps1
@@ -0,0 +1,8 @@
+$path = "WixInstaller\Product.wxs"
+$pattern = 'ProductCode = "\S+"'
+$guid = [guid]::NewGuid().ToString()
+$newValue = 'ProductCode = "'+$guid+'"'
+(Get-Content $path) | 
+Foreach-Object {$_ -replace $pattern,$newValue}  | 
+Out-File $path
+$newValue
