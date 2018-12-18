588c31cff06cee22f351e54bc50dfb34fe4acb4b
Matus Celko <42666212+Winzarten@users.noreply.github.com>
2018-12-18 19:26:51 +0100

    Added telemetry options + feature toggle (#33)

---
+++ Foundation/SecondMonitor.ViewModels/Settings/Model/TelemetrySettings.cs
@@ -0,0 +1,13 @@
+﻿namespace SecondMonitor.ViewModels.Settings.Model
+{
+    using System;
+
+    [Serializable]
+    public class TelemetrySettings
+    {
+        public bool IsFeatureEnabled { get; set; } = false;
+        public bool IsTelemetryLoggingEnabled { get; set; } = false;
+        public int LoggingInterval { get; set; } = 50;
+        public int MaxSessionsKept { get; set; } = 10;
+    }
+}
